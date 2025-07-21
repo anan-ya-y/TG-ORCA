c65cfa618f0474aee0f3bbc96c91b09e5b10be15
Ananya Y <14322650+anan-ya-y@users.noreply.github.com>
2025-07-21 11:36:10 -0500

    create dummy policy

--- crowd_sim/envs/policy/policy_factory.py
+++ crowd_sim/envs/policy/policy_factory.py
@@ -1,6 +1,6 @@
 from crowd_sim.envs.policy.linear import Linear
 from crowd_sim.envs.policy.orca import ORCA
-
+from crowd_sim.envs.policy.tg_orca import TG_ORCA
 
 def none_policy():
     return None
@@ -9,4 +9,5 @@ def none_policy():
 policy_factory = dict()
 policy_factory['linear'] = Linear
 policy_factory['orca'] = ORCA
+policy_factory['tg_orca'] = TG_ORCA
 policy_factory['none'] = none_policy
